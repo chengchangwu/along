ea68833c3d4b4451fafe1dcc6e72ad84c1025047
Sirius Wu <ccwu660601@gmail.com>
2016-02-07 15:44:35 +0800

    changed the stacking context of Repl.

--- components/Repl.js
+++ components/Repl.js
@@ -1,41 +1,57 @@
 import React, { Component } from 'react'
 
+class ReplTextArea extends Component {
+  render() {
+    return <textarea className="repl-textarea"
+      wrap="off"
+      style={{
+        border: "none",
+        height: "17px",
+        width: "8px",
+        left: "192px",
+        top:"34px",
+        resize: "none",
+    }}/>;
+  }
+}
+
+class ReplActiveLine extends Component {
+  render() {
+    return <div
+      className="repl-active-line"
+      style={{
+        height: "17px",
+        top: "34px",
+        left: "0",
+        right: "0",
+    }}/>;
+  }
+}
+class ReplContent extends Component {
+  render() {
+    return <div className="repl-content">
+      <div className="repl-line" style={{
+        height: "17px"
+      }}>: star 42 emit ;  ok</div>
+      <div className="repl-line" style={{
+        height: "17px"
+      }}>star *  ok</div>
+      <div className="repl-line" style={{
+        height: "17px"
+      }}>: stars 0 do star loop ;</div>
+    </div>;
+  }
+}
+
 export default class Repl extends Component {
   render() {
     return <div className="repl" style={{
         fontSize: "16px",
         height: "272px",
     }}>
-      <textarea className="repl-textarea"
-        wrap="off"
-        style={{
-          opacity: 0,
-          border: "none",
-          height: "17px",
-          width: "8px",
-          left: "192px",
-          top:"34px",
-          resize: "none",
-      }}/>
-      <div
-        className="repl-active-line"
-        style={{
-          height: "17px",
-          top: "34px",
-          left: "0",
-          right: "0",
-      }}/>
-      <div className="repl-content">
-        <div className="repl-line" style={{
-          height: "17px"
-        }}>: star 42 emit ;  ok</div>
-        <div className="repl-line" style={{
-          height: "17px"
-        }}>star *  ok</div>
-        <div className="repl-line" style={{
-          height: "17px"
-        }}>: stars 0 do star loop ;</div>
-      </div>
+      <ReplActiveLine />
+      <ReplContent />
+      <ReplTextArea />
     </div>
   }
 }
