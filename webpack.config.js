c952d24208bb5a63b2d6d75567fcdb9cee5a09b0
Sirius Wu <ccwu660601@gmail.com>
2016-05-31 18:55:58 +0800

    moved javascript code into src/.

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path');
 var webpack = require('webpack');
 module.exports = {
   entry: [
-    './index'
+    './src/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
